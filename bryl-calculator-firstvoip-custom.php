8cbde8ccc8ceb48eecdc3c35e484971e4d3eb942
mistergromov27 <40799663+mistergromov27@users.noreply.github.com>
2019-04-15 13:42:15 +0300

    Update bryl-calculator-firstvoip-custom.php

--- bryl-calculator-firstvoip-custom.php
+++ bryl-calculator-firstvoip-custom.php
@@ -18,6 +18,7 @@
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * change
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
